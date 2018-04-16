5db30d3393c273b32304f7303ba74ff63ad623e4
Roberto Sousa <beto.sousa22@gmail.com>
2018-04-16 23:33:27 +0100

    force spaces

--- src/controllers/api/api.ts
+++ src/controllers/api/api.ts
@@ -9,7 +9,12 @@ import { Response, Request, NextFunction } from "express";
  * GET /api
  * List of API examples.
  */
-export const index = (req: Request, res: Response) => {
+export const index = (req : Request, res : Response) => {
+	const arr = [ 1,2,3 ];
+	if ( true ) {}
+	// const func = (value : any) => {
+	// 	console.log(""); // roberto
+	// }
 	res.status(200).json({success: true, message: "get the api message again"});
 	return;
 };
