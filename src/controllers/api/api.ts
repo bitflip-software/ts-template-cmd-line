00cddbc004e95624b39e32ceb3bcc171eaee7e7c
Roberto Sousa <beto.sousa22@gmail.com>
2018-04-16 20:48:50 +0100

    api controller

---
+++ src/controllers/api/api.ts
@@ -0,0 +1,15 @@
+"use strict";
+
+import async from "async";
+import request from "request";
+import { Response, Request, NextFunction } from "express";
+
+
+/**
+ * GET /api
+ * List of API examples.
+ */
+export const index = (req: Request, res: Response) => {
+	res.status(200).json({success: true, message: "get the api messaga"});
+	return;
+};
