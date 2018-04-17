c31a0108ced1e60ff5ed0299190516dcaaa91819
Roberto Sousa <beto.sousa22@gmail.com>
2018-04-17 18:30:19 +0100

    lint the api

--- src/controllers/api/api-routes.ts
+++ src/controllers/api/api-routes.ts
@@ -1,11 +1,11 @@
-import express = require("express");
-import API = require("./api");
+import express = require( "express" );
+import API = require( "./api" );
 
 export namespace Routes {
 	export function index () : express.Router {
 		const router = express.Router();
-		router.route("/")
-			.get(API.index);
+		router.route( "/" )
+			.get( API.index );
 
 		return router;
 	}
